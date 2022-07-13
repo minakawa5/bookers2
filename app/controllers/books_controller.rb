e77dbf76da8361b16d0cb6155a92b1bea700cfd3
SoraMinakawa <104938290+minakawa5@users.noreply.github.com>
2022-07-13 15:58:14 +0000

    レイアウト途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def new
+   @books = Book.new
+  end
+
+  def create
+    @books = Book.new(book_params)
+    @books.user_id = current_user.id
+    @books.save
+    redirect_to books_path
   end
 
   def index
@@ -9,5 +17,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+ private
+
+  def book_params
+    params.require(:book).permit(:shop_name, :image, :caption)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  devise_for :users
   get 'users/show'
   get 'users/edit'
   get "/homes/about" => "homes#about", as: "about"
-
-  resources :bookers, only: [:new, :index, :show, :edit]
-  root to: "homes#top"
-  devise_for :users
+  resources :books, only: [:new, :index, :show, :edit, :create] do
+  end
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
